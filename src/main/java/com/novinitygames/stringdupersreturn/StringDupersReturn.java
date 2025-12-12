ad8dcceb18164dd2d41d21a7d1ba2fe1f440352f
Novinity <novinity63@gmail.com>
2025-12-12 13:50:32 +1000

    fabric support

---
+++ src/main/java/com/novinitygames/stringdupersreturn/StringDupersReturn.java
@@ -0,0 +1,11 @@
+package com.novinitygames.stringdupersreturn;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StringDupersReturn implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[StringDupersReturn] Initialized");
+    }
+}
---
+++ src/main/java/com/novinitygames/stringdupersreturn/mixin/TripwireHookBlockMixin.java
@@ -0,0 +1,24 @@
+package com.novinitygames.stringdupersreturn.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TripwireHookBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(TripwireHookBlock.class)
+public abstract class TripwireHookBlockMixin {
+    @WrapOperation(
+            method = "update",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1)
+            )
+    )
+    private static boolean returnStringDuper(BlockState instance, Block block, Operation<Boolean> original) {
+        return true;
+    }
+}
