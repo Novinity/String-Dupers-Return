5c3ab4829434aaee96dbbccdce2b8aaedda1d323
Novinity <novinity63@gmail.com>
2026-03-29 09:52:43 +1000

    updated to 26.1

--- src/main/java/com/novinitygames/stringdupersreturn/mixin/TripwireHookBlockMixin.java
+++ src/main/java/com/novinitygames/stringdupersreturn/mixin/TripwireHookBlockMixin.java
@@ -2,23 +2,25 @@ package com.novinitygames.stringdupersreturn.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.TripwireHookBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TripWireHookBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-@Mixin(TripwireHookBlock.class)
+@Mixin(TripWireHookBlock.class)
 public abstract class TripwireHookBlockMixin {
     @WrapOperation(
-            method = "update",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"),
+            method = "calculateState",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/block/state/BlockState;is(Ljava/lang/Object;)Z"),
             slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1)
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", ordinal = 1)
             )
     )
-    private static boolean returnStringDuper(BlockState instance, Block block, Operation<Boolean> original) {
+    private static boolean returnStringDuper(BlockState instance, Object o, Operation<Boolean> original) {
         return true;
     }
 }
